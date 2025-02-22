8ead6b1381b5a7dcd5bd221b0fa5d2252e924bf7
AsdOkuu <asdokuu@yeah.net>
2025-02-22 08:40:22 +0800

    lab3 finished(waiting check)

--- lab3/lab.sh
+++ lab3/lab.sh
@@ -10,6 +10,7 @@ function create_image()
 	# your code here 
 	##########################################
 	docker image build -t node .
+    docker images
 }
 
 function create_nodes()
@@ -18,6 +19,12 @@ function create_nodes()
 	##########################################
 	# your code here 
 	##########################################
+    # Loop to create
+    for h in ${nodes[@]}; do
+        docker container create --cap-add NET_ADMIN --name $h node
+    done
+    # List all
+    docker container list -a
 }
 
 
@@ -27,6 +34,9 @@ function run_nodes()
 	##########################################
 	# your code here, check destroy_nodes() about how to use loop in the script
 	##########################################
+    for h in ${nodes[@]}; do
+        docker container start $h
+    done
 }
 
 function create_links()
@@ -41,6 +51,9 @@ function create_links()
 	##########################################
 	# your code here
 	##########################################
+        ns=`docker inspect -f '{{.State.Pid}}' ${nodes[$i]}`
+        # Expose netns
+        ln -s /proc/$ns/ns/net /var/run/netns/$ns
 	done
 	
 	total=${#links[*]}
@@ -50,6 +63,18 @@ function create_links()
 	##########################################
 	# your code here
 	##########################################
+        # Create linked veth
+        ip link add ${links[$i+1]} type veth peer name ${links[$i+4]}
+        # Configure node1
+        ns=`docker inspect -f '{{.State.Pid}}' ${links[$i]}`
+        ip link set ${links[$i+1]} netns $ns
+        ip netns exec $ns ip link set ${links[$i+1]} up
+        ip netns exec $ns ip addr add ${links[$i+2]} dev ${links[$i+1]}
+        # Configure node2
+        ns=`docker inspect -f '{{.State.Pid}}' ${links[$i+3]}`
+        ip link set ${links[$i+4]} netns $ns
+        ip netns exec $ns ip link set ${links[$i+4]} up
+        ip netns exec $ns ip addr add ${links[$i+5]} dev ${links[$i+4]}
 	done
 }
 
@@ -60,7 +85,9 @@ function stop_nodes()
 	##########################################
 	# your code here
 	##########################################
-
+    for h in ${nodes[@]}; do
+        docker container stop $h
+    done
 }
 
 
@@ -72,6 +99,7 @@ function destroy_nodes()
 	##########################################
 	# your code here
 	##########################################
+        docker container rm $h
 	done
 }
 function destroy_image()
@@ -81,6 +109,7 @@ function destroy_image()
 	##########################################
 	# your code here
 	##########################################
+    docker image rm node
 }
 
 function configure_routes()
@@ -92,13 +121,22 @@ function configure_routes()
 	##########################################
 	# your code here
 	##########################################
+        docker exec -it $h bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+        docker exec -it $h cat /proc/sys/net/ipv4/ip_forward
 	done
 	# add routing rules
 	##########################################
 	# your code here
 	##########################################
 	# h1 -> h2 
+    docker exec -it h1 route add -net 111.0.6.0 netmask 255.255.255.0 gateway 111.0.0.2
+    docker exec -it r1 route add -net 111.0.6.0 netmask 255.255.255.0 gateway 111.0.1.2
+    docker exec -it r2 route add -net 111.0.6.0 netmask 255.255.255.0 gateway 111.0.2.2
+    docker exec -it r3 route add -net 111.0.6.0 netmask 255.255.255.0 gateway 111.0.3.2
 	# h2 -> h1
+    docker exec -it h2 route add -net 111.0.0.0 netmask 255.255.255.0 gateway 111.0.6.1
+    docker exec -it r5 route add -net 111.0.0.0 netmask 255.255.255.0 gateway 111.0.5.1
+    docker exec -it r4 route add -net 111.0.0.0 netmask 255.255.255.0 gateway 111.0.4.1
 }
 
 case $1 in 

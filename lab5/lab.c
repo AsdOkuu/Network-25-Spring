11d07ae6d9cf558335a0e75561fb59f5769be018
AsdOkuu <asdokuu@yeah.net>
2025-04-28 17:32:04 +0800

    lab5 finish basic

--- lab5/lab.c
+++ lab5/lab.c
@@ -49,10 +49,29 @@ int init_connection(const char* local_ip, uint16_t local_port,
      **********************/
 
     // 创建UDP socket
+    if ((ccb->udp_sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        return -1;
+    }
 
     // 绑定本地地址
+    struct sockaddr_in local_addr;
+    memset(&local_addr, 0, sizeof(local_addr));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = inet_addr(ccb->local_ip);
+    local_addr.sin_port = htons(ccb->local_port);
+    
+    if (bind(ccb->udp_sock, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0) {
+        perror("bind failed");
+        close(ccb->udp_sock);
+        return -1;
+    }
 
     // 设置远端地址
+    memset(&ccb->remote_addr, 0, sizeof(ccb->remote_addr));
+    ccb->remote_addr.sin_family = AF_INET;
+    ccb->remote_addr.sin_addr.s_addr = inet_addr(ccb->remote_ip);
+    ccb->remote_addr.sin_port = htons(ccb->remote_port);
 
      /***********************
      * end of your code
@@ -147,12 +166,26 @@ int m_send(int conn_id, const void* buf, size_t len) {
          * start of your code
          **********************/
 
-        
         // 处理接收分组
             // 解析头部
             // 更新ACK数组
                 // 仅对非重传分组更新RTT
                 // 拥塞控制
+        char local_buf[MTP_PAYLOAD_LEN + sizeof(mtp_header_t) + 8];
+        ssize_t recv_len = recvfrom(ccb->udp_sock, local_buf, sizeof(local_buf), 0, NULL, NULL);
+        mtp_header_t *header = (mtp_header_t*)local_buf;
+        if(recv_len > 0) {
+            if(!header->ack_flag) {
+                fprintf(stderr, "Received non-ACK packet, seq_num: %u, payload_len: %u\n",
+                        ntohl(header->seq_num), ntohs(header->payload_len));
+                return -1;
+            }
+            uint32_t seq_num = ntohl(header->seq_num);
+            ccb->acks_received[seq_num] = 1;
+
+            fprintf(stderr, "Conn %d: Received ACK packet, seq_num: %u\n", conn_id,
+                    ntohl(header->seq_num));
+        }
             
         // 滑动窗口
         // 发送分组
@@ -161,7 +194,47 @@ int m_send(int conn_id, const void* buf, size_t len) {
             // 超时或者第一次发送：构造并发送分组
             // 记录发送时间
         // 检查完成度
-        
+        while(ccb->window_base + ccb->window_size < ccb->max_seq
+        && ccb->acks_received[ccb->window_base] == 1) {
+            ccb->window_base++;
+        }
+
+        int complete = 1;
+        for(uint32_t i = ccb->window_base; i < ccb->window_base + ccb->window_size && i < ccb->max_seq; i++) {
+            if (ccb->retrans_records[i] == 0) {
+                mtp_header_t *header = (mtp_header_t*) local_buf;
+                int payload_len;
+                if(len > (i + 1) * MTP_PAYLOAD_LEN) {
+                    payload_len = MTP_PAYLOAD_LEN;
+                } else {
+                    payload_len = len - i * MTP_PAYLOAD_LEN;
+                }
+                header->seq_num = htonl(i);
+                header->ack_flag = 0;
+                header->payload_len = htons(payload_len);
+                memcpy(header + 1, buf + i * MTP_PAYLOAD_LEN, payload_len);
+                ssize_t sent_len = sendto(ccb->udp_sock, &local_buf, sizeof(local_buf), 0,
+                                          (struct sockaddr*)&ccb->remote_addr, sizeof(ccb->remote_addr));
+                if (sent_len < 0) {
+                    fprintf(stderr, "Sendto failed");
+                    return -1;
+                }
+
+                fprintf(stderr, "Conn %d: Sent packet, seq_num: %u, payload_len: %u\n", conn_id,
+                        ntohl(header->seq_num), ntohs(header->payload_len));
+
+                ccb->retrans_records[i] = 1;
+            }
+            if (ccb->acks_received[i] == 0) {
+                complete = 0;
+            }
+        }
+
+        if(complete) {
+            fprintf(stderr, "All packets sent\n");
+            break;
+        }
+
         /***********************
          * end of your code
          **********************/
@@ -201,8 +274,11 @@ int m_recv(int conn_id, void* buf, size_t len) {
 
     // 计算最大序列号
     uint32_t max_seq = 0;
+    max_seq = (len + MTP_PAYLOAD_LEN - 1) / MTP_PAYLOAD_LEN;
     
     // 设置非阻塞模式
+    int flags = fcntl(ccb->udp_sock, F_GETFL, 0);
+    fcntl(ccb->udp_sock, F_SETFL, flags | O_NONBLOCK);
     
     /***********************
      * end of your code
@@ -223,11 +299,48 @@ int m_recv(int conn_id, void* buf, size_t len) {
             // 发送ACK
             // 更新接收记录
             // 更新接收进度
+        char local_buf[MTP_PAYLOAD_LEN + sizeof(mtp_header_t) + 8];
+        ssize_t recv_len = recvfrom(ccb->udp_sock, local_buf, sizeof(local_buf), 0, NULL, NULL);
+        mtp_header_t *header = (mtp_header_t*)local_buf;
+        if(recv_len > 0 && ccb->recv_records[ntohl(header->seq_num)] == 0) {
+            if(header->ack_flag) {
+                fprintf(stderr, "Received ACK packet");
+                return -1;
+            }
+            uint32_t seq_num = ntohl(header->seq_num);
+            uint16_t payload_len = ntohs(header->payload_len);
+            memcpy(buf + seq_num * MTP_PAYLOAD_LEN, local_buf + sizeof(mtp_header_t), payload_len);
+            ccb->recv_records[seq_num] = 1;
+            ccb->recv_progress++;
+
+            fprintf(stderr, "Conn %d: Received packet, seq_num: %u, payload_len: %u\n", conn_id,
+                    ntohl(header->seq_num), ntohs(header->payload_len));
+
+            build_ack_packet(header, header->seq_num);
+            sendto(ccb->udp_sock, local_buf, sizeof(mtp_header_t), 0,
+                   (struct sockaddr*)&ccb->remote_addr, sizeof(ccb->remote_addr));
+
+            fprintf(stderr, "Conn %d: Sent ACK packet, seq_num: %u\n", conn_id,
+                    ntohl(header->seq_num));
+        }
 
         // 完成度检查
                 // 首次完成，记录时间
                 // 检查是否超过0.5秒
                     // 如果是，结束接收
+        if(ccb->recv_progress >= max_seq) {
+            if (ccb->recv_complete_time.tv_sec == 0 && ccb->recv_complete_time.tv_usec == 0) {
+                gettimeofday(&ccb->recv_complete_time, NULL);
+            } else {
+                struct timeval now;
+                gettimeofday(&now, NULL);
+                double elapsed = (now.tv_sec - ccb->recv_complete_time.tv_sec) +
+                                 ((double) now.tv_usec - ccb->recv_complete_time.tv_usec) / 1e6;
+                if (elapsed > 0.5) {
+                    break;
+                }
+            }
+        }
         /***********************
          * end of your code
          **********************/
